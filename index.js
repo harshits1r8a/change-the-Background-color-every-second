d363c4e5fca392c134f02984f881c5ce97996274
harshits1r8a <harshsharma.h8a1r3@gmail.com>
2023-10-25 18:11:25 +0530

    first commit using only html and css

---
+++ index.js
@@ -0,0 +1,25 @@
+
+
+const start = document.getElementById('start')
+const stop = document.getElementById('stop')
+
+const colorChange =  function(){
+  const red = parseInt(Math.random()*255+1)
+  const blue = parseInt(Math.random()*255+1)
+  const green = parseInt(Math.random()*255+1)
+  
+  const color = `rgba(${red},${blue},${green})`
+  document.body.style.backgroundColor = color
+}
+
+let myinterval = ''
+
+start.addEventListener('click',(e) => {
+  myinterval = setInterval(colorChange,1000)
+  console.log("start")
+})
+
+stop.addEventListener('click',(e) => { 
+  clearInterval(myinterval)
+  console.log("stop")
+})
